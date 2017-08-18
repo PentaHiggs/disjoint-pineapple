de56bc777760664ed1c9f4f4c27d23ee1219ed0e
PentaHiggs <Ichiroska89@yahoo.com>
2017-08-18 18:42:50 -0400

    Added timestamp to logging, removed lineID

--- src/logging.cpp
+++ src/logging.cpp
@@ -5,6 +5,9 @@
 #include <fstream>
 
 // Boost includes
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+#include <boost/log/support/date_time.hpp>
+#include <boost/log/expressions.hpp>
 #include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/sinks/sync_frontend.hpp>
@@ -23,16 +26,25 @@ namespace logging_ = boost::log;
 
 static void my_formatter(logging_::record_view const& rec, logging_::formatting_ostream& strm)
 {
-    // Get the LineID attribute value and put it into the stream
-    strm << logging_::extract< unsigned int >("LineID", rec) << ": ";
-    strm << logging_::extract< int >("Line", rec) << ": ";
+	// Add timestamp information
+	const boost::posix_time::ptime &pt = *logging_::extract< boost::posix_time::ptime >("TimeStamp", rec);	
+    strm << pt.date() << " " << pt.time_of_day().hours() << ":" << pt.time_of_day().minutes();
+	strm << ":" << std::setw(2) << std::setfill('0') << pt.time_of_day().seconds() << " | ";
+
+	// Get the LineID attribute value and put it into the stream
+    //strm << logging_::extract< unsigned int >("LineID", rec) << ": ";
     logging_::value_ref< std::string > fullpath = logging_::extract< std::string >("File", rec);
-    strm << boost::filesystem::path(fullpath.get()).filename().string() << ": ";
+    strm << boost::filesystem::path(fullpath.get()).filename().string();
+	
+	// Get the line number and put it into stream
+	strm << "[";
+	strm << std::setw(3);
+    strm << logging_::extract< int >("Line", rec) << "]: ";
 
     // The same for the severity level.
     // The simplified syntax is possible if attribute keywords are used.
     strm << "<" << rec[logging_::trivial::severity] << "> ";
-
+	
     // Finally, put the record message to the stream
     strm << rec[expr::smessage];
 }
