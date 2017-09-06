5ffe4a95b6956ebe2883882f23e29559c558e64d
PentaHiggs <Ichiroska89@yahoo.com>
2017-09-05 23:47:09 -0400

    Created a class for performing OCR tasks, still havne't fully tested it through.

--- src/main.cpp
+++ src/main.cpp
@@ -1,109 +1,132 @@
 #include <iostream>
 #include <cstdlib>
-
 #include "logging.hpp"
 
-// includes:
-// allheaders.h
-// baseapi.h
-// basedir.h
-// dict.h
-// openclwrapper.h
-// renderer.h
-// simdetect.h
-// strings.h
-// tprintf.h
-
-// Contains all of the headers for leptonica functionality (class pix defined there)
-#include <leptonica/allheaders.h>
-
-// For renderer functionality
-#include <tesseract/renderer.h>
-
-// Contains the class definitions for tesseract::TessBaseApi.  Requires using std::string in unichar.h
+#include <leptonica/allheaders.h>  // Contains all of the headers for leptonica functionality (class pix defined there)
+#include <tesseract/renderer.h> // For renderer functionality
 #include<string>
 using std::string;
-#include <tesseract/baseapi.h>
-
-// Requires some complicated nonsense involving dict.h which you can't get from include headers
+#include <tesseract/baseapi.h>// Contains the class definitions for tesseract::TessBaseApi.  Requires using std::string in unichar.h
 // Include this file in order to use GlobalDawgCache()
-///#include "dict.h"
+///#include <tesseract/dict.h>
+#include <tesseract/publictypes.h> // Include this in order to get access to tesseract settings enums
 
-// Include this in order to get access to tesseract settings enums
-#include <tesseract/publictypes.h>
-
+#include<unordered_map>
+#include<memory>
 
 using std::cout;
 using std::cerr;
 using std::endl;
+using std::string;
 
-int main() {
-	// Call to create the DawgCache object before TessBaseAPI object
-	// to ensure proper order of destructor calls when exiting function
-	// tesseract::Dict::GlobalDawgCache();
+typedef std::unordered_map<std::string, std::string> dict;
 
-	// This is how tesseract's command line utility initializes tesseract.  By making
-	// it static, we place it in the data segment instead of the stack, which is
-	// useful sometimes for large variables.
-	// static tesseract::TessBaseAPI api;
+std::string getString (const std::string &string, const dict &dictionary, const std::string &default_string = "") {
+	auto found_string_it = dictionary.find(string);
+	if (found_string_it != dictionary.end()) // Means that the setting exists
+		return *found_string_it;
+	else
+		return default_string;
+}
 
-	loglib::logger lg;
-	loglib::init();
+class NewspaperPageClassifier {
+	public:
+		NewspaperPageClassifier(dict* settings) : settings_(settings) {};
+		void changeSettings(dict* settings);
+		std::string findSetting(const std::string &string, const std::string &default_string="") {
+			return getString(string, *settings_, default_string)
+		int classifyFile(std::string file);
+		int init();
+
+		// No copying/assignment of this object.
+		NewspaperPageClassifier(const NewspaperPageClassifier) = delete;
+		NewspaperPageClassifier& operator=(const NewspaperPageClassifier&) = delete;
+
+	private:
+		std::unique_ptr<dict> settings_;
+		bool is_initialized_;
+		loglib::logger lg_;
+		tesseract::TessBaseAPI api_;
+		std::string currentFile_;
+		std::string tessdata_parent_dir_;
+		std::string language_;
+		long int total_pages_rendered_
+}
+
+void NewspaperPageClassifier::ChangeSettings {
+	for(auto const& setting : settings) 
+		settings_->insert(setting);
+	return;
+}	
+
+
+// TODO(andy): Specialize this function to use URLS and load pix files from FILE objects?
+int NewspaperPageClassifier::classifyFile(std::string file, std::string name = "") {
 	
-	EZ_LOG(lg, loglib::debug) << "Tesseract start";
-
-	tesseract::TessBaseAPI api;
-
-	const char* outputName = "purr_TESS_ONLY";
-	api.SetOutputName(outputName);
-
-	// Location of tessdata directory
-	const char* tessdata_parent_dir = "/usr/local/share/tessdata/";
-
-	// Language to be used.  We want English.
-	//const char* language = "eng";
-
-	// Filename of image to be processed.
-	const char* image = "inputs/seq-2.jp2";
-
-	// Engine mode.  We want to use both the neural network and tesseract classic
-	// to fall back on if it doesn't work out.
-	auto engineMode = tesseract::OcrEngineMode::OEM_TESSERACT_LSTM_COMBINED;
-	//auto engineMode = tesseract::OcrEngineMode::OEM_TESSERACT_ONLY;
-
-	int ret = api.Init(tessdata_parent_dir, NULL, engineMode);
-
-	if (ret) {
-		cout << "Tesseract intialization failure, exiting" << endl;
-		return EXIT_FAILURE;
+	// Initialize a renderer with a proper file name.
+	if (name.empty()) {
+		name = findSetting("Session Name")
+		name +=	" : ";
+		name += file;
+		name += " : ";
+		name += std::to_string(total_pages_rendered_);
 	}
 
-	// Page segmentation mode.  There's quite a few options here, but it seems
-	// that PM_AUTO_OSD does everything, which is ideal.
-	// If we do the splitting of text into columns on our own, we'd call
-	// PSM_SINGLE_COLUMN instead
-	api.SetPageSegMode(tesseract::PageSegMode::PSM_AUTO_OSD);
-
-	/*
-	 * Only needed if image isn't being set elsewhere
-	 *
-	Pix* pix_image = pixRead(image);
-	if(!pix_image) {
-		cerr << "Could not read image file: " << image << endl;
-		return EXIT_FAILURE;
-	}
-	
-	api.setImage(pix_image);
-	*/
-
+	// TODO(andy): Check if hocr_font_info boolean can be set beforehand without needing extraction
 	bool font_info;
-	api.GetBoolVariable("hocr_font_info", &font_info);
-	tesseract::TessResultRenderer* renderer = new tesseract::TessTsvRenderer(outputName);
-
-	bool success = api.ProcessPages(image, NULL, 0, renderer);
-	if(!success) {
-		cout << "Processing stage failed." << endl;
-		return EXIT_FAILURE;
+	api_.GetBoolVariable("hocr_font_info", &font_info);
+	std::unique_ptr<tesseract::TessResultRenderer> renderer = new tesseract::TessTsvRenderer(name);
+	
+	// Do the processing
+	if(!renderer->BeginDocument(name)) {
+		EZ_LOG(lg_, "Renderer initialization failure");
+		return -1;
 	}
-	EZ_LOG(lg, loglib::debug) << "Tesseract end";
+	// TODO(andy): Move renderer initialization and closing to constructor and destructor, maybe? 
+	Pix *pix = pixRead(file);
+	if(!ProcessPage(pix, total_pages_rendered_, currentFile_, NULL, 0, renderer.get())) {
+		EZ_LOG(lg_, 
+	pixDestroy(&pix);
+	if(!renderer->EndDocument()) {
+		EZ_LOG(lg_, "Renderer destruction failure");
+		return -1;
+	return 0;	
+}
+
+// Initializes the interal OCR classifier.  Call before ClassifyFile
+int NewspaperPageClassifier::init() {
+	lg_ = loglib::logger();
+	loglib::init();
+	EZ_LOG(lg_, "Tesseract start.  Session name: "
+				+ findSetting("Session Name"));
+	total_pages_rendered_ = 0;
+
+	language_ = findSetting("Language", "eng");
+
+	// Default tessdata directory should be /usr/local/share/tessdata/
+	tessdata_parent_dir_ = findSetting("Tessdata Directory",
+		*dict, "/usr/local/share/tessdata/");
+
+	// OEM_TESSERACT_LSTM_COMBINED uses LSTM, but falls back on old TESSERACT if it struggles
+	int failure = api_.Init(tessdata_parent_dir, language_,
+			tesseract::OcrEngineMode::OEM_TESSERACT_LSTM_COMBINED);
+
+	if (failure) {
+		EZ_LOG(lg_, "Tessearct Failed to Init().  Aborting initialization");
+		return -1;}
+	
+	// For parsing newspaper pages, PSM_AUTO is most appropriate page segmentation mode.
+	api_.SetPageSegMode(tesseract::PageSegMode::PSM_AUTO);
+	
+	EZ_LOG(lg_, "Tesseract initialization successful");
+	is_initialized_ = true;
+	return 0;
+}
+
+
+int main(int argn, const char** argv) {
+	
+	NewspaperPageClassifier npc;
+	npc.init();
+	
 }
