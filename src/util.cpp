b51bdde176eeca90c3101b1866850cf32415e150
PentaHiggs <Ichiroska89@yahoo.com>
2017-09-28 17:13:44 -0400

    Added a new command-line option parsing module.  It has not been integrated to main.  However, wrote unit tests for the module, and it performs as epxected, passing both tests.

--- src/util.cpp
+++ src/util.cpp
@@ -1,3 +1,94 @@
-#include"util.hpp"
+#include "util.hpp"
+#include <iostream>
+#include <getopt.h>
 
-// Really poor, I know.  Templated function had to be moved.
+namespace util {
+void print_help() {
+	std::cout <<
+		"disjoint-pineapple\n"
+		"\n"
+		"Synopsis:\n"
+		"	disjoint-pineapple [OPTIONS] DATE1 DATE2\n"
+		"\n"
+		"Description:\n"
+		"	Program for searching through issues of the Chicago Tribune between the dates DATE1 and\n"
+		"	DATE2, inclusive, with the dates given in YYYY-MM-DD format, and running OCR on them\n"
+		"\n"
+		"Usage:\n"
+		"	disjoint-pineapple 1859-12-15 1865-03-30\n"
+		"	disjoint-pineapple -o 4 1912-03-22 1915-10-09\n"
+		"	disjoint-pineapple -b 10 -o 1 1860-01-01 1860-12-30\n"
+		"	disjoint-pineapple --num_ocr_instances 3 --num_pages_to_classify 5 1933-04-01 1933-04-29\n"
+		"\n"
+		"Options:\n"
+		"	-o --num_ocr_instances			Number of OCR instances to run.  Should not exceed number\n"
+		"		of cores on machine.  Set by default to 2.\n"
+		"	-b --buffer_queue_max_size		This value will almost never have to be modified.  However,\n"
+		"		in the case of a very unstable/unreliable connection, this value can be increased so\n"
+		"		that more files are pre-emptively downloaded.									\n"
+		"	-n --num_pages_to_classify		Sets the number of pages to classify per newspaper issue.\n"
+		"		Typically the most important content is in the first few pages, so this is set to a\n"
+		"		default of 2.  Can be increased if you want to parse more newspaper pages per input\n"
+		"	-h --help\n"
+		"		Prints this help message\n";
+}
+
+
+
+bool parse_args(int argc, char* const* argv, dict& parsed_args) {
+	
+	struct option long_options[] =
+	{
+		{"help",					no_argument,		0, 'h'},	
+		{"num_ocr_instances",		optional_argument,	0, 'o'},
+		{"buffer_queue_max_size",	optional_argument,	0, 'b'},
+		{"num_pages_to_classify",	optional_argument,	0, 'n'},
+		{0, 0, 0, 0}
+	};
+
+	char c = 0;
+	int option_index = 0;
+	while( (c = getopt_long(argc, argv, "ho:b:n:", long_options, &option_index)) !=-1 ){
+		switch (c) {
+			case 0:
+			break;
+			case 'o':
+				if(atoi(optarg)==0) {
+					std::cout << "num_ocr_instances must be integer > 0 .  See --help for usage advice" << std::endl;
+				} else {
+					parsed_args["num_ocr_instances"] = optarg;
+				} 
+				break;
+			case 'b':
+				if(atoi(optarg)==0) {
+					std::cout << "buffer_queue_max_size must be integer > 0.  See --help for usage advice" << std::endl;
+				} else {
+					parsed_args["buffer_queue_max_size"] = optarg;
+				}
+				break;
+			case 'n':
+				if(atoi(optarg)==0) {
+					std::cout << "num_pages_to_classify must be integer > 0.  See --help for usage advice" << std::endl;
+				} else {
+					parsed_args["num_pages_to_classify"] = optarg;
+				}
+				break;
+			case 'h':
+				print_help();
+				return false;
+			default:
+				std::cout << "Unsuccessful argument parse.  Type --help as argument to see usage";
+				return false;
+		}
+	}
+
+	if(argc - optind != 2) {
+		std::cout << "wrong number of position argumens (the dates) given.  See --help for syntax expectations" << std::endl;
+		return false;
+	} else {
+		parsed_args["first_date"] = argv[optind];
+		parsed_args["second_date"] = argv[optind+1];
+	}
+	return true;
+}
+} // namespace util
---
+++ tests/util_test.cpp
@@ -0,0 +1,87 @@
+#define BOOST_TEST_MODULE util_test
+#define BOOST_TEST_DYN_LINK
+
+#include "util.hpp"
+#include <boost/test/unit_test.hpp>
+#include <vector>
+
+std::string showDictDifference (util::dict reference, util::dict actual) {
+	std::string differenceString;
+	for(const auto& entry : reference) {
+		auto in_other = actual.find(entry.first);
+		if (in_other==actual.end()) {
+			// Entry does not exist
+			differenceString += entry.first;
+			differenceString += " does not exist in dict\n";
+		} else {
+			// Key found
+			if (entry.second != in_other->second) {
+				// Value is unequal
+				differenceString += "Value mismatch in ";
+				differenceString += entry.first;
+				differenceString += " ;  got ";
+				differenceString += in_other->second;
+				differenceString += " while expecting ";
+				differenceString += entry.second;
+				differenceString += "\n";
+			}
+		// No problem encountered
+		}
+	}
+	return differenceString;
+}
+
+BOOST_AUTO_TEST_CASE(parse_args_plain)
+{
+	char arguments_[][64] = {
+		"disjoint-pineapple",
+		"1930-12-10", "1931-04-09"};
+	size_t nArgs = 3;
+
+	// getopt is fucking stupid, holy shit.  Only reason I need to do this.  Terrible interface
+	char* arguments[nArgs];
+	for (size_t i = 0; i < nArgs; i++) {arguments[i] = arguments_[i];}
+	
+	util::dict expected_output = {
+		{"first_date",			"1930-12-10"},
+		{"second_date",			"1931-04-09"}
+	};
+
+	util::dict actual_output;
+	bool success = util::parse_args(nArgs, arguments, actual_output);
+	
+	BOOST_CHECK_MESSAGE( success, "parse_args failed on basic input");
+	BOOST_CHECK_MESSAGE( expected_output == actual_output, showDictDifference(expected_output, actual_output));
+}
+
+BOOST_AUTO_TEST_CASE(parse_args_with_options)
+{
+	char arguments_[][64] = {
+		"disjoint-pineapple",
+		"-o", "8",
+		"-b", "2",
+		"-n", "7",
+		"1924-12-14", "1930-01-29"};
+
+	size_t nArgs = 9;
+
+	// Same workaround for getopt
+	char* arguments[nArgs];
+	for (size_t i=0; i < nArgs; i++) {arguments[i] = arguments_[i];}
+	
+	util::dict expected_output = {
+		{"num_ocr_instances",		"8"},
+		{"buffer_queue_max_size",	"2"},
+		{"num_pages_to_classify",	"7"},
+		{"first_date",				"1924-12-14"},
+		{"second_date",				"1930-01-29"}
+	};
+
+	util::dict actual_output;
+	bool success = util::parse_args(nArgs, arguments, actual_output);
+
+	BOOST_CHECK_MESSAGE( success, "parse_args failed on input with options" );
+	BOOST_CHECK_MESSAGE( expected_output == actual_output , showDictDifference(expected_output, actual_output));
+
+}
+
